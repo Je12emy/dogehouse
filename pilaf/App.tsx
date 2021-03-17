5d54e4601281bb7ff6a2783505bae52b572860f4
DIO <jeremyzelaya@hotmail.es>
2021-03-17 17:14:54 -0600

    Added fix for loading UI

--- pilaf/App.tsx
+++ pilaf/App.tsx
@@ -15,6 +15,11 @@ import { useTokenStore } from "./src/module/auth/useTokenStore";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./src/navigators/rootNavigator";
 
+// TODO: Implement logic for switching between storybook UI and app UI, check: https://pusher.com/tutorials/storybook-react-native#setting-up-storybook
+// To hide/show the storybook UI, uncomment the following line of code:
+// The Welcome story was causing some trouble so it has been commented out from: storybook/stories/index.js
+export { default } from "./storybook";
+
 export const App: React.FC = () => {
   const loadTokens = useTokenStore((state) => state.loadTokens);
   const isTokenStoreReady = useTokenStore(
@@ -77,7 +82,4 @@ StyleSheet.create({
   },
 });
 
-// TODO: Implement logic for switching between storybook UI and app UI, check: https://pusher.com/tutorials/storybook-react-native#setting-up-storybook
-// To hide/show the storybook UI, uncomment the following line of code:
-// The Welcome story was causing some trouble so it has been commented out from: storybook/stories/index.js
-export { default } from "./storybook";
+// export default App;
